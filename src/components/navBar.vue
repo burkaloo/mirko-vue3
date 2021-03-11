5b67499a2da0b63d50de3e06210a419034da02ea
James Ong <burkedev@gmail.com>
2021-03-12 00:06:14 +0800

    inital

---
+++ src/components/navBar.vue
@@ -0,0 +1,67 @@
+<template>
+<nav class="navbar navbar-expand-md navbar-light bg-pink">
+  <div class="container-fluid">
+    <div v-if="open" class="w-100 cover" :style="'height: calc(100vh - ' + spacer +'px);'" @click="close"></div>
+    <button class="navbar-toggler" @click="opennav">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <a class="navbar-brand" href="#">Navbar</a>
+    <div v-show="open" :class="collapseClass" :style="'top: '+spacer+'px;'">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+</template>
+
+
+<script>
+export default {
+  name: 'navBar',
+  props:{
+    spacer: String
+  },
+  data(){
+    return {
+      open: false
+    }
+  },
+  methods:{
+    opennav(){
+      this.open = !this.open
+    },
+    close(msg){
+      this.open = false
+    }
+  },
+  computed:{
+    collapseClass(){
+      if(this.open){
+        return "navbar-collapse navbar-collapse-vue bg-pink w-100 p-2"
+      } else{
+        return "navbar-collapse bg-pink"
+      }
+    }
+  },
+}
+</script>
+
+
+<style>
+  .navbar-collapse-vue{
+    position: fixed;
+    z-index: 5;
+    left: 0;
+  }
+  .cover{
+    position: fixed;
+    left: 0;
+    z-index: 4;
+  }
+</style>
---
+++ src/components/sizeChart.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="text-center">
+    <h1 class="text-center">Size Chart</h1>
+    <table class="table mt-3">
+      <thead>
+        <tr>
+          <th>Size</th>
+          <th>Waist</th>
+          <th>Hip</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>S</td>
+          <td>22-25</td>
+          <td>33-35</td>
+        </tr>
+        <tr>
+          <td>M</td>
+          <td>25-28</td>
+          <td>35-37</td>
+        </tr>
+        <tr>
+          <td>L</td>
+          <td>28-31</td>
+          <td>37-39</td>
+        </tr>
+        <tr>
+          <td>XL</td>
+          <td>31-34</td>
+          <td>39-41</td>
+        </tr>
+      </tbody>
+    </table>
+    <div class="text-left">
+      <small>*measurements in inches</small>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'sizeChart',
+}
+</script>
