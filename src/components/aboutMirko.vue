8594cda99812647ed3947381c622241c04a5415f
James Ong <burkedev@gmail.com>
2021-03-18 21:18:11 +0800

    foxed footer and about text align

--- src/components/aboutMirko.vue
+++ src/components/aboutMirko.vue
@@ -1,14 +1,20 @@
 <template>
-    <div style="max-width: 500px" class="text-justify">
+    <div style="max-width: 450px" class="text-center p-4">
       <div class="text-center py-5">
-        <img src="./photos/mirko-logo.png" class="p-3" alt="">
+        <img src="./photos/mirko-logo.png" class="p-3 b-ml" alt="">
       </div>
-      <h2 class="header-font my-5 text-center">About Mirko</h2>
-      <p>Mirko is the brainchild of two full-time working women with the desire to create something to help fellow women experience hassle-free periods. We noticed how menstruation causes inconvenience and disruption at work not only for ourselves but also a lot of our peers - with constant visits to the washroom to check our backsides for accidental leaks and the fear of staining our clothes and chairs. This is the reason we decided to create our very first product under Mirko - the Period Panties (now Mirko’s Flow Underwear). These were designed to keep women comfortable and leak-free all day long without the bulky feeling and constant worry. In the process, we also discovered how little our options are for sustainable period essentials in the Philippines and how limited our collective knowledge is on menstruation as opposed to our western counterparts. We then decided to fully commit Mirko to breaking this taboo in Asian culture and celebrate menstruation as a natural part of being a woman - something to be proud of without fear of judgement.
+      <h2 class="header-font my-5">About Mirko</h2>
+
+      <p class="d-md-none">Mirko is the brainchild of two full-time working women with the desire to create something to help fellow women experience hassle-free periods. We noticed how menstruation causes inconvenience and disruption at work not only for ourselves but also a lot of our peers - with constant visits to the washroom to check our backsides for accidental leaks and the fear of staining our clothes and chairs. This is the reason we decided to create our very first product under Mirko - the Period Panties (now Mirko’s Flow Underwear). These were designed to keep women comfortable and leak-free all day long without the bulky feeling and constant worry. In the process, we also discovered how little our options are for sustainable period essentials in the Philippines and how limited our collective knowledge is on menstruation as opposed to our western counterparts. We then decided to fully commit Mirko to breaking this taboo in Asian culture and celebrate menstruation as a natural part of being a woman - something to be proud of without fear of judgement.
+      </p>
+      <p class="d-none d-md-block text-just">Mirko is the brainchild of two full-time working women with the desire to create something to help fellow women experience hassle-free periods. We noticed how menstruation causes inconvenience and disruption at work not only for ourselves but also a lot of our peers - with constant visits to the washroom to check our backsides for accidental leaks and the fear of staining our clothes and chairs. This is the reason we decided to create our very first product under Mirko - the Period Panties (now Mirko’s Flow Underwear). These were designed to keep women comfortable and leak-free all day long without the bulky feeling and constant worry. In the process, we also discovered how little our options are for sustainable period essentials in the Philippines and how limited our collective knowledge is on menstruation as opposed to our western counterparts. We then decided to fully commit Mirko to breaking this taboo in Asian culture and celebrate menstruation as a natural part of being a woman - something to be proud of without fear of judgement.
       </p>
       <br><br><br>
-      <h2 class="header-font my-5 text-center"> Our Commitment</h2>
-      <p>
+      <h2 class="header-font my-5"> Our Commitment</h2>
+      <p class="d-md-none">
+        mirko seeks to be an innovative leader for women’s essentials. By targeting basic needs and providing only the most functional and top quality products, the brand stands to improve women’s overall quality of life - without all the fuss. We promise to champion this movement in our daily lives as a women-led team and ensure that our solutions remain sustainable and beneficial not only for the current setting but also for the everyday women of the future.
+      </p>
+      <p class="d-none d-md-block text-just">
         mirko seeks to be an innovative leader for women’s essentials. By targeting basic needs and providing only the most functional and top quality products, the brand stands to improve women’s overall quality of life - without all the fuss. We promise to champion this movement in our daily lives as a women-led team and ensure that our solutions remain sustainable and beneficial not only for the current setting but also for the everyday women of the future.
       </p>
     </div>
@@ -24,8 +30,9 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-img{
+.b-mnl{
   max-width: 120px;
   height: auto;
 }
+
 </style>
