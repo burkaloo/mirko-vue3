e73b7009fb3140f6ba183c3de2f3b68ed77a9ea0
James Ong <burkedev@gmail.com>
2021-04-09 22:46:07 +0800

    checkout

--- src/views/checkout.vue
+++ src/views/checkout.vue
@@ -1,6 +1,13 @@
 <template>
-    <div class="" style="height:1650px">
-      <iframe :src="url" width="100%" height="100%"></iframe>
+    <div class="" style="height:1650px; position: relative">
+      <div class="d-flex justify-content-center" style="margin-top: 300px;">
+        <div class="spinner-border" role="status">
+          <span class="visually-hidden">Loading...</span>
+        </div>
+      </div>
+      <div  style="position: absolute; top: 0; z-index:2">
+        <iframe :src="url" width="100%" height="100%"></iframe>
+      </div>
     </div>
 
 </template>
