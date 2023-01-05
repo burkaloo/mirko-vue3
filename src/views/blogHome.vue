65a68300718482fcbbef112b5b02f43c1d71ed61
burkaloo <burkedev@gmail.com>
2023-01-05 10:39:37 +0800

    blog home changes

--- src/views/blogHome.vue
+++ src/views/blogHome.vue
@@ -1,16 +1,15 @@
 <template>
   <div>
     <div class="container py-5">
-      <h2 class="text-center py-5 font-didot">Mirko PH Blog</h2>
+      <h2 class="text-center py-5 font-didot">Mirko Essentials Blog</h2>
       
       <div class="row row-cols-1 row-cols-md-2 row-cols-xl-3">
         <div v-for="post in posts" class="col px-5" :key="post.slug">
           <div :style="{backgroundImage: 'url('+ post.banner +')', width: '100%', paddingTop: '60%', backgroundPosition: 'center', backgroundSize: 'cover', backgroundRepeat: 'no-repeat'}">
           </div>
-          <h5 class="mt-3">{{post.title}}</h5>
+          <router-link :to="'/blog/' + post.slug" class="mt-3 text-decoration-underline fs-5">{{post.title}}</router-link>
           <p class="fw-1">{{post.teaser}}</p>
           
-          <router-link :to="'/blog/' + post.slug">READ MORE</router-link>
         </div>
 
       </div>
